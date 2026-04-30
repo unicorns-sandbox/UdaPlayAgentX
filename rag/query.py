14eb13ae348164d364c39848edeaad4097329839
unicorns-sandbox <97492562+unicorns-sandbox@users.noreply.github.com>
2026-04-30 14:33:26 +0800

    Update query.py

--- rag/query.py
+++ rag/query.py
@@ -1,7 +1,21 @@
-from rag.ingest import load_and_ingest
+from rag.vector_store import VectorStoreManager
 
-db = load_and_ingest()
 
-def semantic_search(query):
-    results = db.query(query)
-    return results["documents"]
+def search_games(query):
+    vs = VectorStoreManager()
+    results = vs.query(query, n_results=5)
+
+    output = []
+
+    for i in range(len(results["documents"][0])):
+        metadata = results["metadatas"][0][i]
+
+        output.append({
+            "title": metadata["title"],
+            "genre": metadata["genre"],
+            "developer": metadata["developer"],
+            "platform": metadata["platform"],
+            "release_date": metadata["release_date"],
+        })
+
+    return output
